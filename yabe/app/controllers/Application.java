7fc311ea27560176fb43f18acdf12175a2ee9d86
kekeliu <1042311019@qq.com>
2013-11-14 13:53:43 +0800

    add the pages

--- yabe/app/controllers/Application.java
+++ yabe/app/controllers/Application.java
@@ -2,15 +2,19 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import java.util.*;
-
 import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
-        render();
+    	//最新的博客
+    	Post frontPost = Post.find("order by postedAt desc").first();
+    	
+    	//过去的博客
+    	List<Post> olderPosts = Post.find(
+    			"order by postedAt desc").from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
 
 }
